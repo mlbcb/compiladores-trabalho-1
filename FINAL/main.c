50d9c3b979d3f7e45df529e686c4aa16e970ac0e
sara <116955380+sarasls@users.noreply.github.com>
2025-11-09 21:26:33 +0000

    upload final with bugs

---
+++ FINAL/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.tab.h"
+extern int yyparse(void);
+extern FILE* yyin;
+extern void printAST();
+
+int main(int argc, char** argv) {
+    --argc; ++argv;
+    if (argc != 0) {
+        yyin = fopen(*argv, "r");
+        if (!yyin) {
+        printf("'%s': could not open file\n", *argv);
+        return 1;
+        }
+    } //  yyin = stdin
+    if (yyparse() == 0) {
+        printf("Abstract Sintax Tree:\n");
+        printAST();
+    }
+
+    fclose(yyin);
+    return 0;
+
+}
